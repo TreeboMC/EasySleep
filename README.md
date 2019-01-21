6a286bda1448ec8e019b15ce6f7f599245a7abd0
ShakeforProtein <github@taktimer.net>
2019-01-21 21:44:41 +1000

    Initial Commit - Added Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# EasySleep
+Allows you to toggle between needing one player to sleep, or half of the online players on a world to sleep.
+
+## Commands
+/easysleep - Toggles between one or half to sleep.
+
+## Permissions
+easysleep.toggle - Allows a player to use the /easysleep command - Defaults to OP
---
+++ src/main/java/me/shakeforprotein/easysleep/EasySleep.java
@@ -0,0 +1,108 @@
+package me.shakeforprotein.easysleep;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasySleep extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("EasySleep enabled");
+        this.getCommand("easysleep").setExecutor(this);
+        getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        if (getConfig().get("requireHalfToSleep") == null) {
+            getConfig().set("requireHalfToSleep", 0);
+        }
+        for (World world : Bukkit.getWorlds()) {
+            getConfig().set(world.getName(), 0);
+        }
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+    @EventHandler
+    private void onPlayerSleepEvent(PlayerBedEnterEvent e) {
+        Player p = e.getPlayer();
+        if(getConfig().getInt(p.getWorld().getName()) < 0){getConfig().set(p.getWorld().getName(), 0);}
+        int democracy = getConfig().getInt("requireHalfToSleep");
+        int playersOnWorld = e.getPlayer().getWorld().getPlayers().size();
+        long currentTime = p.getWorld().getFullTime();
+        long currentDay = currentTime / 24000;
+        long newDay = currentDay * 24000 + 6000;
+
+        if (playersOnWorld % 2 != 0){
+            playersOnWorld = playersOnWorld +1;
+        }
+
+        if ((p.getWorld().getTime() > 12860 && p.getWorld().getTime() < 23000) || p.getWorld().isThundering()) {
+            if (democracy == 1) {
+                    getConfig().set(p.getWorld().getName(), getConfig().getInt(p.getWorld().getName()) + 1);
+
+
+                    if (getConfig().getInt(p.getWorld().getName()) >= ((playersOnWorld / 2))) {
+                        p.getWorld().setFullTime(newDay);
+                        for (Player player : p.getWorld().getPlayers()) {
+                            player.sendMessage(ChatColor.GOLD + "Wakey wakey and welcome to a brand new day");
+                            getConfig().set(p.getWorld().getName(), 0);
+                        }
+                    } else {
+                        for (Player player : p.getWorld().getPlayers()) {
+                            player.sendMessage(ChatColor.GOLD + "There are " + ChatColor.GREEN + getConfig().get(p.getWorld().getName()) + ChatColor.GOLD + " players of a required " + ChatColor.RED + (playersOnWorld / 2) + ChatColor.GOLD + " who are in bed");
+                        }
+                    }
+            }
+            else {
+                    p.getWorld().setFullTime(newDay);
+                    for (Player player : p.getWorld().getPlayers()) {
+                        player.sendMessage(ChatColor.GOLD + "Wakey wakey and welcome to a brand new day");
+                    }
+                getConfig().set(p.getWorld().getName(), 0);
+
+            }
+        } else {
+            p.sendMessage("You may only sleep at night or during a storm");
+        }
+
+    }
+
+    @EventHandler
+    private void onPlayerLeaveBedEvent(PlayerBedLeaveEvent e) {
+        Player p = e.getPlayer();
+        getConfig().set(p.getWorld().getName(), getConfig().getInt(p.getWorld().getName()) - 1);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player){Player p = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("easysleep") && sender.hasPermission("easysleep.toggle")) {
+            if (getConfig().getInt("requireHalfToSleep") == 1) {
+                getConfig().set("requireHalfToSleep", 0);
+                p.sendMessage("It now only requires one player to sleep");
+                p.sendMessage(getConfig().getInt(((Player) sender).getWorld().getName()) + "");
+            } else {
+                getConfig().set("requireHalfToSleep", 1);
+                p.sendMessage("It now requires half of a worlds players sleep");
+                p.sendMessage(getConfig().getInt(((Player) sender).getWorld().getName()) + "");
+            }
+            saveConfig();
+        }}
+        return true;
+    }
+
+}
